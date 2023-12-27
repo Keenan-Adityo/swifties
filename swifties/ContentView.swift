09850f7113f870834a63d7c771bea7fda11a7f80
keenan <keenan167@gmail.com>
2023-12-27 20:13:09 +0700

    swift create

---
+++ swifties/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swifties
+//
+//  Created by Keenan Adityo on 27/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, test!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swifties/swiftiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftiesApp.swift
+//  swifties
+//
+//  Created by Keenan Adityo on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
